642c07b0f80ee75789518b4eed5d33d8836ab795
Nando Biaccalli <nandobiaccalli@gmail.com>
2024-09-08 22:47:08 +0200

    page d'accueil + mise en place agency details

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './global.css'
+import { DM_Sans } from "next/font/google";
+import { Metadata } from "next";
+import { ThemeProvider } from "@/providers/theme-provider";
+import ModalProvider from "@/providers/modal-provider";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Plura",
+  description: "All in one Agency Solution",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <html lang="en" suppressHydrationWarning>
+    <body className={font.className}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <ModalProvider>
+          {children}
+        </ModalProvider>
+      </ThemeProvider>
+    </body>
+  </html>;
+}
